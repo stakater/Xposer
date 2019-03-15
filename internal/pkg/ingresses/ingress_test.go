a83f7088d6b93de479466232236cd193e60e8194
Niels Kjøller Hansen <kjoller@septima.dk>
2019-03-15 13:58:12 +0100

    Test for default backend (#18)

--- internal/pkg/ingresses/ingress_test.go
+++ internal/pkg/ingresses/ingress_test.go
@@ -64,7 +64,7 @@ func TestAddTLSInfo(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			AddTLSInfo(tt.args.ingress, tt.args.ingressName, tt.args.ingressHost)
 			if len(tt.args.ingress.Spec.TLS) < 1 {
-				t.Errorf("TLS Not added to ingress = %v, want %v", tt.args.ingress)
+				t.Errorf("TLS Not added to ingress = %v", tt.args.ingress)
 			}
 		})
 	}
@@ -83,14 +83,14 @@ func TestShouldAddTLS(t *testing.T) {
 		{
 			name: "should retrun true",
 			args: args{
-				ingressConfig: createIngressConfigMap("true"),
+				ingressConfig: createIngressTlsConfigMap("true"),
 			},
 			want: true,
 		},
 		{
 			name: "should retrun true",
 			args: args{
-				ingressConfig: createIngressConfigMap("false"),
+				ingressConfig: createIngressTlsConfigMap("false"),
 			},
 			want: false,
 		},
@@ -99,6 +99,73 @@ func TestShouldAddTLS(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := ShouldAddTLS(tt.args.ingressConfig, tt.args.defaultTLS); got != tt.want {
 				t.Errorf("ShouldAddTLS() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAddDefaultBackend(t *testing.T) {
+	type args struct {
+		ingress     *v1beta1.Ingress
+		serviceName string
+		servicePort int
+	}
+	tests := []struct {
+		name     string
+		args     args
+		modified *v1beta1.Ingress
+		want     string
+	}{
+		{
+			name: "Should Add Default Backend",
+			args: args{
+				ingress:     createIngressForDefaultBackend(),
+				serviceName: "test-service",
+				servicePort: 1234,
+			},
+			modified: createIngressForDefaultBackend(),
+			want: "test-service",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			AddDefaultBackend(tt.args.ingress, tt.args.serviceName, tt.args.servicePort)
+			if got := (tt.args.ingress.Spec.Backend.ServiceName); got != tt.want {
+				t.Errorf("Default Backend Not added to ingress = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestShouldAddDefaultBackend(t *testing.T) {
+	type args struct {
+		ingressConfig            map[string]interface{}
+		defaultDefaultBackend    bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "should return true",
+			args: args{
+				ingressConfig: createIngressDefaultBackendConfigMap("true"),
+			},
+			want: true,
+		},
+		{
+			name: "should return false",
+			args: args{
+				ingressConfig: createIngressDefaultBackendConfigMap("false"),
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ShouldAddDefaultBackend(tt.args.ingressConfig, tt.args.defaultDefaultBackend); got != tt.want {
+				t.Errorf("ShouldAddDefaultBackend() = %v, want %v", got, tt.want)
 			}
 		})
 	}
@@ -120,9 +187,24 @@ func createIngressForTLS() *v1beta1.Ingress {
 	}
 }
 
-func createIngressConfigMap(tlsValue string) map[string]interface{} {
+func createIngressForDefaultBackend() *v1beta1.Ingress {
+	return &v1beta1.Ingress{
+		ObjectMeta: meta_v1.ObjectMeta{
+			Name: "DefaultBackend-Ingress",
+		},
+	}
+}
+
+func createIngressTlsConfigMap(tlsValue string) map[string]interface{} {
 	ingressConfig := make(map[string]interface{})
 	ingressConfig[constants.TLS] = tlsValue
+
+	return ingressConfig
+}
+
+func createIngressDefaultBackendConfigMap(defaultBackendValue string) map[string]interface{} {
+	ingressConfig := make(map[string]interface{})
+	ingressConfig[constants.DEFAULTBACKEND] = defaultBackendValue
 
 	return ingressConfig
 }
